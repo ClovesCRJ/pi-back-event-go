6ef70f62bf6233617673f469ba03e4c7b62e7ad0
Cloves Rodrigues <cloves.crj@gmail.com>
2022-10-12 17:37:26 -0300

    Fixing Auth Guards

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, HttpCode, HttpStatus, UseGuards, Req } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/v1/users')
-@UseGuards(AuthGuard('jwt'))
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
@@ -19,22 +18,25 @@ export class UserController {
   //   return this.userService.findAll();
   // }
 
-  @Get(':id')
-  async findOne(@Param('id', new ParseUUIDPipe) id: string) {
+  @Get('me')
+  @UseGuards(AuthGuard('jwt'))
+  async findOne(@Req() req: any) {
     return await this.userService.findOneOrFail({
-      where: { id },
+      where: { id: req.user.id },
       select: ['id', 'email', 'first_name', 'last_name', 'created_at', 'updated_at'],
     });
   }
 
   @Patch(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(AuthGuard('jwt'))
   async update(@Param('id', new ParseUUIDPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
     return await this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(AuthGuard('jwt'))
   async remove(@Param('id', new ParseUUIDPipe) id: string) {
     return await this.userService.remove(id);
   }
