db383ea6379cea0627b5d19e6a63a652c46a68df
Cloves Rodrigues <cloves.crj@gmail.com>
2022-10-30 00:30:03 -0300

    Prepare to Deploy on Heroku

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/v1/swagger', app, document);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
